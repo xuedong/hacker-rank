1e5afce4c5a888078045021cd1e8decd1a3ca5d2
xuedong <shang.xuedong@yahoo.fr>
2018-02-09 04:19:02 +0800

    First version of Check.

--- Competitions/Week of Code 36/Ways to Give a Check/Check.m
+++ Competitions/Week of Code 36/Ways to Give a Check/Check.m
@@ -1,23 +1,96 @@
 #import <Foundation/Foundation.h>
 
-int check(char[8][8] board) {
+int queen(int qi, int qj, int ki, int kj) {
+    if (qi == ki || qj == kj) {
+        return 1;
+    }
+    else if (abs(qi-ki) == abs(qj-kj)) {
+        return 1;
+    }
     
+    return 0;
 }
 
-int main(int argc, const char * argv[]) {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+int rook(int ri, int rj, int ki, int kj) {
+    if (ri == ki || rj == kj) {
+        return 1;
+    }
     
-    int t;
-    scanf("%i", &t);
-    for (int scenario = 0; scenario < t; scenario++) {
-        char board[8][8];
-        for (int board_i = 0; board_i < 8; board_i++) {
-            for (int board_j = 0; board_j < 8; board_j++) {
-                scanf("%c", &board[board_i][board_j]);
+    return 0;
+}
+
+int bishop(int bi, int bj, int ki, int kj) {
+    if (abs(bi-ki) == abs(bj-kj)) {
+        return 1;
+    }
+    
+    return 0;
+}
+
+int knight(int ni, int nj, int ki, int kj) {
+    if (abs(ni-ki) == 2 && abs(nj-kj) == 1) {
+        return 1;
+    }
+    else if (abs(ni-ki) == 1 && abs(nj-kj) == 2) {
+        return 1;
+    }
+    
+    return 0;
+}
+
+int check(int ki, int kj, char board[8][9]) {
+    int ways = 0;
+    int pi = 1;
+    int pj = 0;
+    for (int j = 1; j < 9; j++) {
+        if (board[pi][j] == 'P') {
+            //printf("%i %i ", pi, j);
+            pj = j;
+            if (queen(0, pj, ki, kj)) {
+                ways += 1;
+            }
+            if (rook(0, pj, ki, kj)) {
+                ways += 1;
+            }
+            if (bishop(0, pj, ki, kj)) {
+                ways += 1;
+            }
+            if (knight(0, pj, ki, kj)) {
+                ways += 1;
             }
         }
     }
     
-    [pool drain];
+    return ways;
+}
+
+int main(int argc, const char * argv[]) {
+    //NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    int t;
+    scanf("%i", &t);
+    for (int scenario = 0; scenario < t; scenario++) {
+        char board[8][9];
+        for (int board_i = 0; board_i < 8; board_i++) {
+            for (int board_j = 0; board_j < 9; board_j++) {
+                scanf("%c", &board[board_i][board_j]);
+                //printf("%c %i %i \n", board[board_i][board_j], board_i, board_j);
+            }
+        }
+        
+        int ki, kj;
+        for (int board_i = 1; board_i < 8; board_i++) {
+            for (int board_j = 1; board_j < 9; board_j++) {
+                if (board[board_i][board_j] == 'k') {
+                    ki = board_i;
+                    kj = board_j;
+                }
+            }
+        }
+        int ways = check(ki, kj, board);
+        printf("%i\n", ways);
+    }
+    
+    //[pool drain];
     return 0;
 }
