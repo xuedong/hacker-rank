e6d90695ae150c6269f5cfc0922ff535162f4ec0
xuedong <shang.xuedong@yahoo.fr>
2020-11-08 14:36:16 +0100

    Update interview prep kit
    Add naive fibonacci.py

---
+++ Tutorials/Interview Preparation Kit/Recursion and Backtracking/Recursion: Fibonacci Number/fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+    # Write your code here.
+
+n = int(input())
+print(fibonacci(n))
+
