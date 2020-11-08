1ca810349bb9850b7a89fcd25718e8c9ecc6dd81
xuedong <shang.xuedong@yahoo.fr>
2020-11-08 14:07:54 +0100

    Rename file

---
+++ Tutorials/Interview Preparation Kit/Stacks and Queues/Largest Rectangle/largest_rectangle2.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the largestRectangle function below.
+def largestRectangle(h):
+    stack = []
+    best = 0
+    
+    i = 0
+    while i < len(h):
+        if not stack or h[stack[-1]] <= h[i]:
+            stack.append(i)
+            i += 1
+        else:
+            top = stack.pop()
+            best = max(best, h[top] * (i - stack[-1] - 1 if stack else i))
+    
+    while stack:
+        top = stack.pop()
+        best = max(best, h[top] * (i - stack[-1] - 1 if stack else i))
+    return best
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    h = list(map(int, input().rstrip().split()))
+
+    result = largestRectangle(h)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
