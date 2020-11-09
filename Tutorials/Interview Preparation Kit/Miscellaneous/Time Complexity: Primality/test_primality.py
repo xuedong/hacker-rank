e0f27a322ed96ad5b31a0ae1482e32c7d9bda476
xuedong <shang.xuedong@yahoo.fr>
2020-11-09 01:57:57 +0100

    Fix a bug in test_primality.py

--- Tutorials/Interview Preparation Kit/Miscellaneous/Time Complexity: Primality/test_primality.py
+++ Tutorials/Interview Preparation Kit/Miscellaneous/Time Complexity: Primality/test_primality.py
@@ -10,7 +10,7 @@ import sys
 def primality(n):
     if n == 1:
         return 'Not prime'
-    for i in range(2, int(math.sqrt(n))):
+    for i in range(2, int(math.sqrt(n))+1):
         if n % i == 0:
             return 'Not prime'
     return 'Prime'
