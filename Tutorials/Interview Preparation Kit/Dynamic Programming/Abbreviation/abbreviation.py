e7a92b1850cbea20f341bcff8fcfaf13e248731d
xuedong <shang.xuedong@yahoo.fr>
2020-12-03 00:47:33 +0100

    Update interview prep kit - dp
    Add abbreviation.py

---
+++ Tutorials/Interview Preparation Kit/Dynamic Programming/Abbreviation/abbreviation.py
@@ -0,0 +1,66 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the abbreviation function below.
+def abbreviation(a, b):
+    A = [[None for j in range(len(b))] for i in range(len(a))]
+
+    if a[0].upper() == b[0]:
+        A[0][0] = True
+    else:
+        A[0][0] = False
+    upper_encountered = a[0].isupper()
+    for i in range(1, len(a)):
+        if a[i].isupper() and upper_encountered:
+            A[i][0] = False
+        elif a[i].isupper() and not upper_encountered and a[i] == b[0]:
+            A[i][0] = True
+            upper_encountered = True
+        elif a[i].isupper() and not upper_encountered and a[i] != b[0]:
+            A[i][0] = False
+            upper_encountered = True
+        elif a[i].islower() and a[i].upper() == b[0] and not upper_encountered:
+            A[i][0] = True
+        else:
+            A[i][0] = A[i-1][0]
+
+    for j in range(1, len(b)):
+        A[0][j] = False
+
+    for i in range(1, len(a)):
+        for j in range(1, len(b)):
+            if a[i].upper() == b[j] and a[i].islower():
+                A[i][j] = A[i-1][j-1] or A[i-1][j]
+            elif a[i].upper() == b[j] and a[i].isupper():
+                A[i][j] = A[i-1][j-1]
+            elif a[i].upper() != b[j] and a[i].islower():
+                A[i][j] = A[i-1][j]
+            else:
+                A[i][j] = False
+ 
+    if A[len(a)-1][len(b)-1]:
+        return "YES"
+    return "NO"
+ 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        a = input()
+
+        b = input()
+
+        result = abbreviation(a, b)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
