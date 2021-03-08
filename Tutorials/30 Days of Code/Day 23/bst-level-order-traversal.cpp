0448fcf4de6a0a25b13906341acd78b7ec679486
xuedong <shang.xuedong@yahoo.fr>
2021-03-08 16:50:45 +0100

    Update 30 days of code
    Add bst traversal

---
+++ Tutorials/30 Days of Code/Day 23/bst-level-order-traversal.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstddef>
+#include <queue>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *left, *right;
+        Node (int d) {
+            data = d;
+            left = right = NULL;
+        }
+};
+
+class Solution{
+    public:
+  		Node* insert(Node* root, int data){
+            if (root == NULL) {
+                return new Node(data);
+            }
+            else{
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                   cur = insert(root->right, data);
+                   root->right = cur;
+                 }           
+           return root;
+           }
+        }
+
+	void levelOrder(Node * root){
+        //Write your code here
+        queue<Node *> q;
+        Node *current = root;
+        q.push(current);
+        
+        while (!q.empty()) {
+            cout << current->data << " ";
+            if (current->left) {
+                q.push(current->left);
+            }
+            if (current->right) {
+                q.push(current->right);
+            }
+            q.pop();
+            current = q.front();
+        }
+	}
+};//End of Solution
+
+
+int main(){
+    Solution myTree;
+    Node* root = NULL;
+    int T, data;
+    cin >> T;
+    while (T-- > 0) {
+        cin>>data;
+        root= myTree.insert(root,data);
+    }
+    myTree.levelOrder(root);
+    return 0;
+}
+
