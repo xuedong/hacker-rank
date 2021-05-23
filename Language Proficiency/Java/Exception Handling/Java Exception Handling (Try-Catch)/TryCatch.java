797f7b3fa88ced0f6ee9b331be3949f581a72392
xuedong <shang.xuedong@yahoo.fr>
2021-05-23 12:30:26 +0200

    Update Java Exception Handling
    Add try-catch

---
+++ Language Proficiency/Java/Exception Handling/Java Exception Handling (Try-Catch)/TryCatch.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(x/y);
+        } catch (ArithmeticException | InputMismatchException e) {
+            if (e instanceof ArithmeticException) {
+                System.out.println("java.lang.ArithmeticException: / by zero");
+            } else {
+                System.out.println(e.getClass().getName());
+            }
+        }
+    }
+}
+
