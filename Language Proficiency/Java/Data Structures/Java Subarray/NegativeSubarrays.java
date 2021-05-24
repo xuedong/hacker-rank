43bddf76461ebeb66617881c3e45397679525cc8
xuedong <shang.xuedong@yahoo.fr>
2021-05-24 13:26:13 +0200

    Update Java Data Structures
    Add subarray

---
+++ Language Proficiency/Java/Data Structures/Java Subarray/NegativeSubarrays.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum < 0) count++;
+            }
+            sum = 0;
+        }
+        
+        System.out.println(count);
+    }
+}
+
