0d91bd578fc4573e256759c118df35c40ec660d5
xuedong <shang.xuedong@yahoo.fr>
2021-06-13 00:31:49 +0200

    Update Python Datatypes
    Add nested lists

---
+++ Language Proficiency/Python/Basic Data Types/Nested Lists/nested_lists.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    scores = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        scores.append([name, score])
+
+    second_lowest = sorted(list(set([score for _, score in scores])))[1]
+    print('\n'.join([name for name, score in sorted(scores) if score == second_lowest]))
+
