7a24dd4db47f98eeadeb34c8daa202b8d29e9b26
xuedong <shang.xuedong@yahoo.fr>
2021-06-14 01:28:34 +0200

    Update Java Strings
    Add split and join

---
+++ Language Proficiency/Python/Strings/String Split and Join/split_join.py
@@ -0,0 +1,11 @@
+def split_and_join(line):
+    # write your code here
+    result = line.split(" ")
+    result = "-".join(result)
+    return result
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
+
