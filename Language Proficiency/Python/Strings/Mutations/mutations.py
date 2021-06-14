4dfb301dd62ea4b4401f63b3a17b9e7feab14629
xuedong <shang.xuedong@yahoo.fr>
2021-06-15 01:45:53 +0200

    Update Python Strings
    Add string mutations

---
+++ Language Proficiency/Python/Strings/Mutations/mutations.py
@@ -0,0 +1,13 @@
+def mutate_string(string, position, character):
+    string_list = list(string)
+    string_list[position] = character
+    list_string = ''.join(string_list)
+    return list_string
+
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+
