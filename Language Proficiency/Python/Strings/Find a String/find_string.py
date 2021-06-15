1e58c6c0fd58c981c25aa30aa65cc019aa085697
xuedong <shang.xuedong@yahoo.fr>
2021-06-16 00:41:46 +0200

    Update Python Strings
    Add find strinUpdate Python Strings
    
    Add find string

---
+++ Language Proficiency/Python/Strings/Find a String/find_string.py
@@ -0,0 +1,16 @@
+def count_substring(string, sub_string):
+    str_len = len(string)
+    sub_len = len(sub_string)
+    count = 0
+    for i in range(str_len-sub_len+1):
+        if sub_string == string[i:(i+sub_len)]:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
+
