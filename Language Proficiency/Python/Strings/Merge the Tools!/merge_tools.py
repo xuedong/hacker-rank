2b51089967ff6d2f673b6429b1ef889d37d46d68
xuedong <shang.xuedong@yahoo.fr>
2021-06-22 00:41:41 +0200

    Update Python Strings
    Add merge the tools

---
+++ Language Proficiency/Python/Strings/Merge the Tools!/merge_tools.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+def merge_the_tools(string, k):
+    # your code goes here
+    temp = []
+    length = 0
+    for char in string:
+        if char not in temp:
+            temp.append(char)
+        length += 1
+        if length == k:
+            print(''.join([char for char in temp]))
+            temp = []
+            length = 0
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
+
