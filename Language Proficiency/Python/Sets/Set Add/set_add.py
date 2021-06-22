416d7c70897d732dd5b84403fb21c4650af971fd
xuedong <shang.xuedong@yahoo.fr>
2021-06-23 00:29:52 +0200

    Update Python Sets
    Add set .add()

---
+++ Language Proficiency/Python/Sets/Set Add/set_add.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    stamps = set()
+
+    num = int(input())
+    for _ in range(num):
+        stamp = input()
+        stamps.add(stamp)
+
+    print(len(stamps))
+
