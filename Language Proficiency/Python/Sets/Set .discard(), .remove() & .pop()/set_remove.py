7197c74146552f949405e522f03a8bf4c5b7d823
xuedong <shang.xuedong@yahoo.fr>
2021-06-24 00:38:59 +0200

    Update Python Sets
    Add set remove operations

---
+++ Language Proficiency/Python/Sets/Set .discard(), .remove() & .pop()/set_remove.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    n = int(input())
+    s = set(map(int, input().split()))
+    N = int(input())
+
+    for _ in range(N):
+        eval("s.{0}({1})".format(*input().split()+['']))
+
+    print(sum(s))
+
