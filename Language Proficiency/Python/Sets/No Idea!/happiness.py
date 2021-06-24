16692c8c20236578e123e9172e35cebfcd5a1593
xuedong <shang.xuedong@yahoo.fr>
2021-06-25 00:33:06 +0200

    Update Python Sets
    Add happiness

---
+++ Language Proficiency/Python/Sets/No Idea!/happiness.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    arr = (map(int, input().split()))
+    like = set(map(int, input().split()))
+    dislike = set(map(int, input().split()))
+
+    print(sum([(i in like) - (i in dislike) for i in arr]))
+
