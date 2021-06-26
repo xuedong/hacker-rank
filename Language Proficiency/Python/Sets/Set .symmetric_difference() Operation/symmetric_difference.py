c9c625872deeb3c838dcba92ba2dc00953582d34
xuedong <shang.xuedong@yahoo.fr>
2021-06-27 00:43:05 +0100

    Update Python Sets
    Add python symmetric difference

---
+++ Language Proficiency/Python/Sets/Set .symmetric_difference() Operation/symmetric_difference.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    n = int(input())
+    subs_a = set(map(int, input().split()))
+    m = int(input())
+    subs_b = set(map(int, input().split()))
+
+    print(len(subs_a^subs_b))
+
