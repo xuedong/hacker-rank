883ec7c0ecb28d935803b89d41fb07fd239020dc
xuedong <shang.xuedong@yahoo.fr>
2021-07-04 13:43:56 +0100

    Update Python Sets
    Add the captain's room

---
+++ Language Proficiency/Python/Sets/The Captain's Room/captain_room.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    K = int(input())
+    arr = map(int, input().split())
+
+    s1 = set()
+    s2 = set()
+    for e in arr:
+        if e in s1:
+            s2.add(e)
+        else:
+            s1.add(e)
+
+    print(s1.difference(s2).pop())
+
