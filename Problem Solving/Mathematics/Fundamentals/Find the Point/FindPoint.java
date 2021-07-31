899e03e35f7da02ed4370f870d47a2420b12e99a
xuedong <shang.xuedong@yahoo.fr>
2021-07-31 01:01:54 +0100

    Update Mathematics Fundamentals

---
+++ Problem Solving/Mathematics/Fundamentals/Find the Point/FindPoint.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findPoint' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER px
+     *  2. INTEGER py
+     *  3. INTEGER qx
+     *  4. INTEGER qy
+     */
+
+    public static List<Integer> findPoint(int px, int py, int qx, int qy) {
+    // Write your code here
+        List<Integer> res = new ArrayList<>();
+        res.add(2*qx-px);
+        res.add(2*qy-py);
+        return res;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, n).forEach(nItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int px = Integer.parseInt(firstMultipleInput[0]);
+
+                int py = Integer.parseInt(firstMultipleInput[1]);
+
+                int qx = Integer.parseInt(firstMultipleInput[2]);
+
+                int qy = Integer.parseInt(firstMultipleInput[3]);
+
+                List<Integer> result = Result.findPoint(px, py, qx, qy);
+
+                bufferedWriter.write(
+                    result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                    + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
