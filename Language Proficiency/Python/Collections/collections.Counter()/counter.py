f101ced0a12c870aa233efeaf5ffa403f1e1b8d2
xuedong <shang.xuedong@yahoo.fr>
2021-08-01 15:49:10 +0200

    Update Python Collections
    Add counter

---
+++ Language Proficiency/Python/Collections/collections.Counter()/counter.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+from collections import Counter
+
+
+if __name__ == "__main__":
+    num_shoes = int(input())
+    shoes = Counter(map(int, input().split()))
+    num_customers = int(input())
+    res = 0
+
+    for i in range(num_customers):
+        size, price = map(int, input().split())
+        if shoes[size]:
+            res += price
+            shoes[size] -= 1
+
+    print(res)
