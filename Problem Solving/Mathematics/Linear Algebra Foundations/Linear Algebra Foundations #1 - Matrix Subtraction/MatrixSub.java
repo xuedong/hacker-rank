1581207dbd3d47249aabfd7f568d00f320918ff6
xuedong <shang.xuedong@yahoo.fr>
2021-08-08 00:44:04 +0200

    Udpate Math LA
    Add matrix subtraction

---
+++ Problem Solving/Mathematics/Linear Algebra Foundations/Linear Algebra Foundations #1 - Matrix Subtraction/MatrixSub.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int res[] = new int[]{-3, -3, -3, -5, -5, -5, -3, -4, 1};
+        for (int i = 0; i <= 8; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
+
