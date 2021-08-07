d3975dd5f539a0088ba235549222f535715d9289
xuedong <shang.xuedong@yahoo.fr>
2021-08-08 00:41:20 +0200

    Update Math Linear Algebra
    Add matrix addition

---
+++ Problem Solving/Mathematics/Linear Algebra Foundations/Linear Algebra Foundations #1 - Matrix Addition/MatrixAddition.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int res[] = new int[]{5, 7, 9, 9, 11, 13, 5, 6, 8};
+        for (int i = 0; i <= 8; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
+
