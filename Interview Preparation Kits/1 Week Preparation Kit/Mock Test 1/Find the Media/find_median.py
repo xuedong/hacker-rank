e72f00635a6c31aa731496385001d69fffbf40b0
xuedong <shang.xuedong@yahoo.fr>
2021-11-06 12:16:30 +0000

    Update 1 week prep kit
    Add mock test 1

---
+++ Interview Preparation Kits/1 Week Preparation Kit/Mock Test 1/Find the Media/find_median.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findMedian' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def findMedian(arr):
+    # Write your code here
+    n = len(arr)
+    arr = sorted(arr)
+    if n % 2 == 0:
+        return (arr[n//2-1] + arr[n//2])/2
+    else:
+        return arr[n//2]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = findMedian(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
