300917ea22a05c2fb22c16b7dd84c07ed8a06872
xuedong <shang.xuedong@yahoo.fr>
2021-12-11 02:24:49 +0000

    Update Math Algebra
    Add combo meal

---
+++ Problem Solving/Mathematics/Algebra/Combo Meal/ComboMeal.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'profit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER b
+     *  2. INTEGER s
+     *  3. INTEGER c
+     */
+
+    public static int profit(int b, int s, int c) {
+    // Return the fixed profit.
+        return b+s-c;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int b = Integer.parseInt(firstMultipleInput[0]);
+
+                int s = Integer.parseInt(firstMultipleInput[1]);
+
+                int c = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = Result.profit(b, s, c);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
