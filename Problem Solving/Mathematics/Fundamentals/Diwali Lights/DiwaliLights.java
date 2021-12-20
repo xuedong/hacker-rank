0fdf9a57fa12e11c33f461765280615b10bec17e
xuedong <shang.xuedong@yahoo.fr>
2021-12-20 15:36:38 +0100

    Update math fundamental
    Add Diwali lights

---
+++ Problem Solving/Mathematics/Fundamentals/Diwali Lights/DiwaliLights.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'lights' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static long lights(int n) {
+    // Write your code here
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = (result * 2) % 100000;
+        }
+        return (result - 1) % 100000;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                long result = Result.lights(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
