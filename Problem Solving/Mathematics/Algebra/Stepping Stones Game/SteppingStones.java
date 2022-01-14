281c7bd9939dbab658d9bfb848c21ac79a795a53
Edouard Leurent <eleurent@gmail.com>
2022-01-15 00:53:28 +0100

    Update math algebra
    Add stepping stones

---
+++ Problem Solving/Mathematics/Algebra/Stepping Stones Game/SteppingStones.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static String solve(long n) {
+    // Write your code here
+        double root = Math.sqrt(1+8*n);
+        if (Math.abs(Math.floor(root) - root) <= 1e-9 && (long) (root-1) % 2 == 0) {
+            return "Go On Bob ".concat(String.valueOf((long) (root-1)/2));
+        } else {
+            return "Better Luck Next Time";
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                String result = Result.solve(n);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
