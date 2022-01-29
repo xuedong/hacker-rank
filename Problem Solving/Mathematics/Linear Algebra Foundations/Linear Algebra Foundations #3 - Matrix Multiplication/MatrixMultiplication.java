fdbdde942a7e32af186aae2783556b5237828972
xuedong <shang.xuedong@yahoo.fr>
2022-01-30 00:38:44 +0100

    Update linear algebra
    Add matrix multiplication

---
+++ Problem Solving/Mathematics/Linear Algebra Foundations/Linear Algebra Foundations #3 - Matrix Multiplication/MatrixMultiplication.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int res[] = new int[]{18, 21, 29, 34};
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
+
