8a7f90ceb2ab45058430feb10379983cd783eb83
xuedong <shang.xuedong@yahoo.fr>
2022-01-30 01:12:09 +0100

    Update linear algebra
    Add matrix power

---
+++ Problem Solving/Mathematics/Linear Algebra Foundations/Linear Algebra Foundations #4 - Matrix Multiplication/MatrixMultiplication.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int[] res = new int[]{30, 36, 45, 45, 54, 67, 15, 18, 22};
+        for (int i = 0; i < 9; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
+
---
+++ Problem Solving/Mathematics/Linear Algebra Foundations/Linear Algebra Foundations #5 - The 100th Power of a Matrix/MatrixPower.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int[] res = new int[]{1, 100, 1, 0, 1};
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
+
