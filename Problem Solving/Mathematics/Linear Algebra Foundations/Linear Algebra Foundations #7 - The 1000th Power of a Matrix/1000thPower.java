fff50ad540e5c5a072078b9ac295b5619b34d098
xuedong <shang.xuedong@yahoo.fr>
2022-01-31 13:16:53 +0100

    Update math linear algebra

---
+++ Problem Solving/Mathematics/Linear Algebra Foundations/Linear Algebra Foundations #7 - The 1000th Power of a Matrix/1000thPower.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int[] res = new int[]{-2999, -9000, 1000, 3001};
+        for (int i = 0; i < 4; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
+
