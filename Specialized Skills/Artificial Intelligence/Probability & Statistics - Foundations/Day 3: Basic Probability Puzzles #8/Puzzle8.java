e1e34211b9b02903bbaad0160a2ba826897e21a6
xuedong <shang.xuedong@yahoo.fr>
2022-05-14 01:52:53 +0200

    Add proba stats puzzle 8

---
+++ Specialized Skills/Artificial Intelligence/Probability & Statistics - Foundations/Day 3: Basic Probability Puzzles #8/Puzzle8.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        // P(M v E) = P(M) + P(E) - P(M ^ E)
+        float pm = 1 - 1/2;
+        float pe = 1 - 2/5;
+        float pme = 1/5;
+        
+        System.out.println("9/10");
+    }
+}
+
